ca677143a995171fdce730f146f9c2d47d04e61d
Kinan Muhammed <kinanmuhammed@hotmail.com>
2013-03-10 13:51:39 +0000

    cv_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CvApp::Application.config.secret_token = 'acf636264e8122d5fc3790a97eeaed1f4cab80d3a8f6107aacb299810aa05ef466103cf0b6a123f529c10b8e74b6d57917e34fcad0de1dfdd9952ab5724b03f1'
